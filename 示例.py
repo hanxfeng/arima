3311052b5961862dc24f2952f3f030daeb8a973f
hanxfeng <3556050019@qq.com>
2024-05-28 20:18:05 +0800

    Update 示例.py

--- 示例.py
+++ 示例.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from 函数 import data_adf,lb,train_pq,train_,bianma
-
+import statsmodels.api as sm
 a=bianma('1.csv')
 data=pd.read_csv('1.csv',encoding=a)
 aa='货量'
 data=data['货量']
 
-d=data_adf(data,'5%')
+d=data_adf(data)
 lb(data)
-p,q=train_pq(data,d)#7,5
-a=train_(data,10,p,d,q,pl=True)
+p,q=train_pq(data,d)
+a=train_(data,10,p,d,q)
+
